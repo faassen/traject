71d08e0ee75c916c2e0e6f91faf2ec793c2f7be6
Martijn Faassen <faassen@startifact.com>
2019-11-14 17:46:50 +0100

    Start of error handling.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::borrow::Cow;
+
+#[derive(Debug, PartialEq)]
+struct Error {}
 
 /// Check whether a variable name is a proper identifier.
 fn is_identifier(s: &str) -> bool {
@@ -10,6 +12,7 @@ fn is_identifier(s: &str) -> bool {
     IDENTIFIER.is_match(s)
 }
 
+#[derive(Debug)]
 struct Step {
     s: String,
     generalized: String,
@@ -19,7 +22,7 @@ struct Step {
 }
 
 impl Step {
-    fn new(s: &str) -> Step {
+    fn new(s: &str) -> Result<Step, Error> {
         lazy_static! {
             static ref PATH_VARIABLE: Regex = Regex::new(r"\{([^}]*)\}").unwrap();
         }
@@ -33,19 +36,24 @@ impl Step {
             .map(|s| s[1..s.len() - 1].to_string())
             .collect();
 
-        Step {
+        for name in &names {
+            if !is_identifier(&name) {
+                return Err(Error {});
+            }
+        }
+        Ok(Step {
             s: s.to_owned(),
             generalized: generalized,
             parts,
             names,
-        }
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use proptest::prelude::*;
+    // use proptest::prelude::*;
 
     #[test]
     fn test_is_identifier() {
@@ -59,7 +67,7 @@ mod tests {
 
     #[test]
     fn test_step_new_no_variables() {
-        let step = Step::new("foo");
+        let step = Step::new("foo").unwrap();
         assert_eq!(step.s, "foo");
         assert_eq!(step.generalized, "foo");
         assert_eq!(step.parts, vec!["foo"]);
@@ -68,7 +76,7 @@ mod tests {
 
     #[test]
     fn test_step_new_one_variable_start() {
-        let step = Step::new("{bar}baz");
+        let step = Step::new("{bar}baz").unwrap();
         assert_eq!(step.s, "{bar}baz");
         assert_eq!(step.generalized, "{}baz");
         assert_eq!(step.parts, vec!["", "baz"]);
@@ -77,7 +85,7 @@ mod tests {
 
     #[test]
     fn test_step_new_one_variable_middle() {
-        let step = Step::new("foo{bar}baz");
+        let step = Step::new("foo{bar}baz").unwrap();
         assert_eq!(step.s, "foo{bar}baz");
         assert_eq!(step.generalized, "foo{}baz");
         assert_eq!(step.parts, vec!["foo", "baz"]);
@@ -86,7 +94,7 @@ mod tests {
 
     #[test]
     fn test_step_new_one_variable_end() {
-        let step = Step::new("foo{bar}");
+        let step = Step::new("foo{bar}").unwrap();
         assert_eq!(step.s, "foo{bar}");
         assert_eq!(step.generalized, "foo{}");
         assert_eq!(step.parts, vec!["foo", ""]);
@@ -95,7 +103,7 @@ mod tests {
 
     #[test]
     fn test_step_new_one_variable_only() {
-        let step = Step::new("{bar}");
+        let step = Step::new("{bar}").unwrap();
         assert_eq!(step.s, "{bar}");
         assert_eq!(step.generalized, "{}");
         assert_eq!(step.parts, vec!["", ""]);
@@ -104,17 +112,23 @@ mod tests {
 
     #[test]
     fn test_step_multiple_variables() {
-        let step = Step::new("foo{bar}baz{qux}frub");
+        let step = Step::new("foo{bar}baz{qux}frub").unwrap();
         assert_eq!(step.s, "foo{bar}baz{qux}frub");
         assert_eq!(step.generalized, "foo{}baz{}frub");
         assert_eq!(step.parts, vec!["foo", "baz", "frub"]);
         assert_eq!(step.names, vec!["bar", "qux"]);
     }
 
-    proptest! {
-        #[test]
-        fn doesnt_crash(s in "\\PC*") {
-            Step::new(&s);
-        }
+    #[test]
+    fn test_step_bad_variable() {
+        let step = Step::new("foo{%$}baz");
+        assert!(step.is_err());
     }
+
+    // proptest! {
+    //     #[test]
+    //     fn doesnt_crash(s in "\\PC*") {
+    //         Step::new(&s).unwrap();
+    //     }
+    // }
 }
